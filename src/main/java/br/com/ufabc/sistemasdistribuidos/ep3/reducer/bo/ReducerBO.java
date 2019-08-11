4cfc60a264b207e671beb5e4147564aea606a05c
Luana Nascimento <lluana13@hotmail.com>
2019-08-11 10:26:46 -0300

    Recebendo urls com os links close #1

---
+++ src/main/java/br/com/ufabc/sistemasdistribuidos/ep3/reducer/bo/ReducerBO.java
@@ -0,0 +1,11 @@
+package br.com.ufabc.sistemasdistribuidos.ep3.reducer.bo;
+
+public class ReducerBO {
+	public ReducerBO() {
+		
+	}
+	
+	public void recebeLinks(String links) {
+		
+	}
+}
---
+++ src/main/java/br/com/ufabc/sistemasdistribuidos/ep3/reducer/tcp/TCPServer.java
@@ -0,0 +1,63 @@
+package br.com.ufabc.sistemasdistribuidos.ep3.reducer.tcp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import br.com.ufabc.sistemasdistribuidos.ep3.reducer.bo.ReducerBO;
+
+public class TCPServer {
+	private ServerSocket server;
+	private ReducerBO reducerbo;
+
+	public TCPServer(String ipAddress) throws Exception {
+		reducerbo = new ReducerBO();
+		
+		if (ipAddress != null && !ipAddress.isEmpty())
+			this.server = new ServerSocket(8082, 1, InetAddress.getByName(ipAddress));
+		else
+			this.server = new ServerSocket(8082, 1, InetAddress.getLocalHost());
+	}
+
+	private void listen() throws Exception {
+		String data = null;
+		Socket client = this.server.accept();
+		String clientAddress = client.getInetAddress().getHostAddress();
+		System.out.println("\r\nNew connection from " + clientAddress);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		StringBuilder builder = new StringBuilder();
+		while ((data = in.readLine()) != null) {
+			System.out.println("\r\nMessage from " + clientAddress + ": " + data);
+			
+			builder.append(data);
+		}
+		reducerbo.recebeLinks(builder.toString());
+	}
+
+	public InetAddress getSocketAddress() {
+		return this.server.getInetAddress();
+	}
+
+	public int getPort() {
+		return this.server.getLocalPort();
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("REDUCER");
+		TCPServer app;
+
+		if (args.length > 0)
+			app = new TCPServer(args[0]);
+		else
+			app = new TCPServer(null);
+
+		System.out.println(
+				"\r\nRunning Server: " + "Host=" + app.getSocketAddress().getHostAddress() + " Port=" + app.getPort());
+
+		app.listen();
+	}
+}
+
